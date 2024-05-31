b6146ff644c2cc1f7ef1282b7913e3388ec86a9c
Bárbara Espinola <Baesp2023@gmail.com>
2024-05-31 18:26:22 -0300

    update vercel

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Login from './Pages/Login'
-import LandingPage from './Pages/LandingPage'
-import DashboardUser from './Pages/Dashboar-User'
-import Navbar from './Components/NavBAr/NavBar'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./Pages/Login";
+import LandingPage from "./Pages/LandingPage";
+import DashboardUser from "./Pages/Dashboar-User";
+import Navbar from "./Components/NavBar/NavBar";
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/user" element={<DashboardUser />} />
-
       </Routes>
-    </Router>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import App from './App'
-import store from './Redux/store'
+import App from './App.tsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import store from './Redux/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -13,5 +13,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
