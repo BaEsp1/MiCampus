fe06588e5613e3fc9fe06ee2d531ce5753df99d6
Bárbara Espinola <Baesp2023@gmail.com>
2024-05-31 18:01:23 -0300

    update

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  },
-  server: {
-    port: 3000
-  }
 })
